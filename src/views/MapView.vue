8509e956a29968536f34127c04dbb81f9588c9db
BelajarDimana <belajardimana@gmail.com>
2023-05-19 20:49:31 +0700

    Update View SideBar

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -13,17 +13,15 @@ const props = defineProps({
 <template>
   <div style="height:90vh; width:100vw">
     <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]" :options="{zoomControl:false}">
-    <button style="z-index: 500;" @click="allList = true"
-    class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">Halo</button>
+    <button style="z-index: 500; width: 6rem; height: 3.5rem;" @click="allList = true"
+    class="bg-neutral-500 hover:bg-neutral-400 text-white font-bold py-2 px-4 border-b-4 border-neutral-700 hover:border-neutral-500 rounded">View All Data:</button>
       <l-tile-layer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         layer-type="base"
         name="OpenStreetMap"
       ></l-tile-layer>
       <l-control-zoom position="topright" ></l-control-zoom>
-      <!-- <l-marker :lat-lng="[47.7515953048815, 8.757179159967961]" /> -->
       <template v-for="post in model" :key="model">
-      <!-- <button style="z-index: 500; color: black;">{{ post.nama }}</button> -->
         <l-marker v-if="post.idFC"  :lat-lng="[post.latitude, post.longitude]" @click="isOpened = true, data = post">
           <l-icon :icon-size= [40,40] icon-url="http://belajardimana.com/gambar/location.png" > </l-icon>
           <!-- <l-popup ref="popup">
@@ -33,44 +31,51 @@ const props = defineProps({
       </template>
     </l-map>
 
-    <VueSidePanel v-model="isOpened" side="left">
-      <Button @click="isOpened=false, data=null, allList=true"
-      class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">Back</Button>
-      <div style="height: 100%;" class="text-black">
-        <div v-if="data">
-          <img v-if="data.linkGambar" class="w-full" :src="data.linkGambar" 
+    <VueSidePanel v-model="isOpened" side="left">  
+        <div style="height: 100%; max-width: 30vw;" class="block bg-white p-3 dark:bg-neutral-700 text-neutral-800 dark:text-neutral-50 text-2xl py-5">
+              <Button @click="isOpened=false, data=null, allList=true"
+            style="width: 2.5rem;">
+          <img src="http://belajardimana.com/gambar/left-arrow.png"/>
+          </Button>
+        <div v-if="data" style="max-width: 80%; padding-left: 4rem;">
+          <h2 v-if="data.nama" class="mb-5"> {{ data.nama }} - {{ data.publisher }}</h2>
+          <img v-if="data.linkGambar" class="w-half shadow-[0_2px_15px_-3px_rgba(0,0,0,1),0_10px_20px_-2px_rgba(0,0,0,0.04)]" :src="data.linkGambar" 
                 alt="Placeholder image">
-          <img v-else class="w-full" src="http://belajardimana.com/gambar/placeholder.png" 
+          <img v-else class="w-half shadow-[0_2px_15px_-3px_rgba(0,0,0,1),0_10px_20px_-2px_rgba(0,0,0,0.04)]" src="http://belajardimana.com/gambar/placeholder.png" 
                 alt="Placeholder image">
-          <h2 v-if="data.nama"> {{ data.nama }} </h2>
-          <h2 v-if="data.publisher"> {{ data.publisher }} </h2>
-          <h2 v-if="data.durasiMinggu"> {{ data.durasiMinggu }} </h2>
-          <h2 v-if="data.alamat"> {{ data.alamat }} </h2>
-          <h2 v-if="data.linkDaftar"> {{ data.linkDaftar }} </h2>
+          <h2 v-if="data.biayaOri" class="my-2 mt-10"> Rp {{ data.biayaOri }}  </h2>
+          <h2 v-if="data.durasiMinggu" class="my-2"> {{ data.durasiMinggu }} Pertemuan Per Minggu </h2>
+          <h2 v-if="data.alamat" class="my-2"> {{ data.alamat }} </h2>
+          <h2 v-if="data.linkDaftar" class="my-2"> {{ data.linkDaftar }} </h2>
         </div>
       </div>    
     </VueSidePanel>
 
     
     <VueSidePanel v-model="allList" side="left">
+      <div v-if="model.length==0" style="height: 100%; padding-top: 50vh;"  class="block bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 text-neutral-800 dark:text-neutral-50 text-3xl py-10">
+        <p>Nothing to show</p>
+    </div>
+    <div v-else style="height: 100%;"  class="block bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 text-neutral-800 dark:text-neutral-50 text-3xl py-10">
+      <h1 class="mb-5">Data Listed:</h1>
+     
         <template v-for="datas in model" :key="model">
-          <div v-if="model.length==0">
-              <p>Nothing to show</p>
-          </div>
-          <div v-else style="height: 100%;" class="text-black">
+          <div>
             <div @click="isOpened=true, allList=false, data = datas">
-              <img v-if="datas.linkGambar" class="w-full" :src="datas.linkGambar" 
-                    alt="Placeholder image">
-              <img v-else class="w-full" src="http://belajardimana.com/gambar/placeholder.png" 
-                    alt="Placeholder image">
-              <h2 v-if="datas.nama"> {{ datas.nama }} </h2>
-              <h2 v-if="datas.publisher"> {{ datas.publisher }} </h2>
-              <h2 v-if="datas.durasiMinggu"> {{ datas.durasiMinggu }} </h2>
-              <h2 v-if="datas.alamat"> {{ datas.alamat }} </h2>
-              <h2 v-if="datas.linkDaftar"> {{ datas.linkDaftar }} </h2>
+              <div class="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 dark:md:hover:bg-neutral-500">
+                <h5 v-if="datas.nama && datas.publisher" class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                  {{ datas.nama }} - {{ datas.publisher }}
+                </h5>
+                <p v-if="datas.alamat" class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                  {{ datas.alamat }}
+                </p>
+                <button></button>
+              </div>
             </div>
-        </div>   
+        </div>
+        
       </template> 
+      </div>
     </VueSidePanel>
     
   </div>
@@ -95,9 +100,11 @@ export default {
       data:null,
       isOpened: false,
       allList:false,
+      check:true,
     };
   },
 };
 </script>
 
-<style></style>
+<style>
+VueSidePanel::-webkit-scrollbar {display:none;}</style>
